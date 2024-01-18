ab31b043f2139f634160deca2d2553d66ac76211
Daniel <buptdaniel2017@gmail.com>
2024-01-18 11:40:33 +0800

    add ABI go file ing

--- config/contract.go
+++ config/contract.go
@@ -1,5 +1,12 @@
 package config
 
 type Contract struct {
-	AlchemyRpcUrl string `mapstructure:"alchemy_rpc_url" json:"alchemy_rpc_url" yaml:"alchemy_rpc_url"`
+	AlchemyRpcUrl          string `mapstructure:"alchemy_rpc_url" json:"alchemy_rpc_url" yaml:"alchemy_rpc_url"`
+	VipcardContractAbi     string `mapstructure:"vipcard_contract_abi" json:"vipcard_contract_abi" yaml:"vipcard_contract_abi"`
+	VipcardName            string `mapstructure:"vipcard_name" json:"vipcard_name" yaml:"vipcard_name"`
+	VipcardSymbol          string `mapstructure:"vipcard_symbol" json:"vipcard_symbol" yaml:"vipcard_symbol"`
+	Erc6551RegistryAddress string `mapstructure:"erc6551_registry_address" json:"erc6551_registry_address" yaml:"erc6551_registry_address"`
+	VipcardAccountAddress  string `mapstructure:"vipcard_account_address" json:"vipcard_account_address" yaml:"vipcard_account_address"`
+	PrivateKey             string `mapstructure:"private_key" json:"private_key" yaml:"private_key"`
+	ChainId                string `mapstructure:"chain_id" json:"chain_id" yaml:"chain_id"`
 }
--- service/pocket.go
+++ service/pocket.go
@@ -15,37 +15,25 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/spf13/viper"
 )
 
 var (
-	vipcardContractABI        = viper.GetString("contract.vipcard_contract_abi")
-	vipcardContractByteCode   = viper.GetString("")
-	vipcardName               = viper.GetString("contract.vipcard_name")
-	vipcardSymbol             = viper.GetString("contract.vipcard_symbol")
-	alchemyRPCURL             = viper.GetString("contract.alchemy_rpc_url")
-	privateKeyHex             = viper.GetString("contract.private_key")
-	erc6551RegistryAddressHex = viper.GetString("contract.erc6551_registry_address")
-	vipcardAccountAddressHex  = viper.GetString("contract.vipcard_account_address")
-	err                       error
-	client                    *ethclient.Client
-	erc6551RegistryAddress    common.Address
-	vipcardAccountAddress     common.Address
-	fromAddress               common.Address
-	privateKey                *ecdsa.PrivateKey
-	chainId                   *big.Int
+	err                    error
+	client                 *ethclient.Client
+	erc6551RegistryAddress common.Address
+	vipcardAccountAddress  common.Address
+	fromAddress            common.Address
+	privateKey             *ecdsa.PrivateKey
+	chainId                *big.Int
 )
 
 func DeployERC721Contract(name string, address string, descripe string) (string, error) {
 	// address是用户钱包地址，即部署这个721合约的地址。目前先实现了由admin账户进行部署，再将owner设为用户钱包地址的部署方式
 	//1.准备工作
 	fromAddress = common.HexToAddress(address)
-	log.Debugf("check1 : %v", erc6551RegistryAddressHex)
-	erc6551RegistryAddress = common.HexToAddress(erc6551RegistryAddressHex)
-	vipcardAccountAddress = common.HexToAddress(vipcardAccountAddressHex)
-	log.Debugf("check2 : %v", alchemyRPCURL)
-	log.Debugf("check3 : %v", global.App.Config.Contract.AlchemyRpcUrl)
-	client, err := ethclient.Dial(alchemyRPCURL)
+	erc6551RegistryAddress = common.HexToAddress(global.App.Config.Contract.Erc6551RegistryAddress)
+	vipcardAccountAddress = common.HexToAddress(global.App.Config.Contract.VipcardAccountAddress)
+	client, err := ethclient.Dial(global.App.Config.Contract.AlchemyRpcUrl)
 	if err != nil {
 		log.Debugf("Failed to create eth client: %v", err)
 	}
@@ -55,29 +43,32 @@ func DeployERC721Contract(name string, address string, descripe string) (string,
 	}
 
 	//2.读取合约的ABI和字节码
-	contractJSON, err := os.ReadFile(vipcardContractABI)
+	contractJSON, err := os.ReadFile(global.App.Config.Contract.VipcardContractAbi)
 	if err != nil {
-		log.Debugf("Failed to read ERC721 contract file: %v", err)
+		log.Debugf("Failed to read ERC721 contract file: %v", contractJSON)
 	}
 
 	var contractData struct {
-		ABI      string `json:"abi"`
-		Bytecode string `json:"bytecode"`
+		ABI      string
+		Bytecode string
 	}
+	//`json:"abi"``json:"bytecode"`
 
 	err = json.Unmarshal(contractJSON, &contractData)
+	log.Debugf("ABI: %v", contractData.ABI)
 	if err != nil {
 		log.Debugf("Failed to unmarshal ERC721 contract JSON: %v", err)
 	}
 
 	//3.构建部署合约的ABI
+	//contractAbi, err := abi.JSON(bytes.NewReader(abiData))
 	parsedABI, err := abi.JSON(strings.NewReader(contractData.ABI))
 	if err != nil {
 		log.Debugf("Failed to parse ABI: %v", err)
 	}
 
 	//4.创建一个新的交易，然后使用您的私钥对交易进行签名
-	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+	privateKey, err := crypto.HexToECDSA(global.App.Config.Contract.PrivateKey)
 	if err != nil {
 		log.Debug(err)
 	}
@@ -101,7 +92,15 @@ func DeployERC721Contract(name string, address string, descripe string) (string,
 	auth.GasPrice = gasPrice
 
 	contractAddress, tx, _, err := bind.DeployContract(
-		auth, parsedABI, common.FromHex(contractData.Bytecode), client, name, vipcardSymbol, erc6551RegistryAddress, vipcardAccountAddress, fromAddress)
+		auth,
+		parsedABI,
+		common.FromHex(contractData.Bytecode),
+		client,
+		name,
+		global.App.Config.Contract.VipcardSymbol,
+		global.App.Config.Contract.Erc6551RegistryAddress,
+		global.App.Config.Contract.VipcardAccountAddress,
+		fromAddress)
 	if err != nil {
 		log.Debug(err)
 	}
